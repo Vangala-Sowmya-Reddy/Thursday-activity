d246b48709dec10419f2fd95cbdeb397faa348c3
Vangala-Sowmya-Reddy <115688191+Vangala-Sowmya-Reddy@users.noreply.github.com>
2022-10-13 11:40:05 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,14 @@
+class ArithmeticOperators{
+public static void main(String[] args)
+{
+int a=10,b=20,c=30;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a%b);
+System.out.println(a*b);
+System.out.println(a>b && b<c);
+System.out.println(a>c & b<c);
+System.out.println(a>c || b<a);
+System.out.println(a>b | b<c);
+}
+}
